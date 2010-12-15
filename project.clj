896be6ec731a29d3b07adb2ffc61367134ffef25
Nicolas Buduroi <nbuduroi@gmail.com>
2010-12-15 18:08:26 -0500

    Updated lein-clojars dependency.

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
                for interactive development."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[lein-clojars "0.5.0"]
+  :dev-dependencies [[lein-clojars "0.6.0"]
                      [swank-clojure "1.2.1"]])
